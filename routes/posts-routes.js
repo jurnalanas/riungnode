1d0c5258fd0aa546fba246114601ecbc62ea56b7
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2019-12-25 11:26:38 +0700

    custom error model

--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_POSTS = [{
@@ -31,9 +33,7 @@ router.get('/:pid', (req, res, next) => {
   })
 
   if (!post) {
-    const error = new Error('Could not find a post for the provided id.');
-    error.code = 404;
-    throw error;
+    throw new HttpError('Could not find a post for the provided id.', 404);
   }
 
   res.json({post});
@@ -47,9 +47,9 @@ router.get('/user/:uid', (req, res, next) => {
   });
 
   if (!post) {
-    const error = new Error('Could not find a post for the provided user id.');
-    error.code = 404;
-    return next(error);
+    return next(
+      new HttpError('Could not find a post for the provided user id.', 404)
+    );
   }
 
   res.json({ post });
